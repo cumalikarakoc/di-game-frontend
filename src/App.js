95b52bbe986393ff923a290ff7a025923d7042c5
MaartenGDev <maarten.grootoonk@gmail.com>
2019-02-20 14:19:33 +0100

    Center user profiles around center point

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import LoginPage from './screens/auth/LoginPage'
-import GameProgress from './screens/dashboard/GameProgress'
+import GameProgressPage from './screens/dashboard/GameProgressPage'
 
 class App extends Component {
   render () {
@@ -10,7 +10,7 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path="/" component={LoginPage}/>
-            <Route exact path="/dashboard" component={GameProgress}/>
+            <Route exact path="/dashboard" component={GameProgressPage}/>
           </Switch>
         </div>
       </Router>
---
+++ src/screens/dashboard/components/PlayerProfile.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const PlayerProfile = ({size, position, level, avatarUrl}) => {
+  return (
+    <div className='rounded-full absolute'
+         style={{backgroundSize: 'cover', backgroundImage: `url('${avatarUrl}')`, width: size.width, height: size.height, left: `${position.x}px`, top: `${position.y}px`}}>
+      <div className='relative w-full h-full'>
+        <div className='absolute pin-b flex justify-center w-full h-full items-end -mb-4'>
+          <span className='inline-block p-2 bg-grey rounded-full'>Level {level}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+PlayerProfile.propTypes = {
+  level: PropTypes.string.isRequired,
+  avatarUrl: PropTypes.string.isRequired,
+  position: PropTypes.shape({
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
+  }),
+  size: PropTypes.shape({
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
+  }),
+}
+
+export default PlayerProfile
